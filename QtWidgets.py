803982d2bf9634153fd3f121f1ade9a2b1f566a0
Lucas Silva <79523835+guilucasv@users.noreply.github.com>
2024-04-24 22:02:33 -0300

    Update QtWidgets.py

--- QtWidgets.py
+++ QtWidgets.py
@@ -1,68 +1,69 @@
-# O básico sobre Signal e Slots (eventos e documentação)
+# Trabalhando com classes e herança no PySide6
 import sys
 
 from PySide6.QtCore import Slot
 from PySide6.QtWidgets import (QApplication, QGridLayout, QMainWindow,
                                QPushButton, QWidget)
 
-app = QApplication(sys.argv)
-window = QMainWindow()
-central_widget = QWidget()
-window.setCentralWidget(central_widget)
-window.setWindowTitle('Minha janela bonita')
 
-botao1 = QPushButton('Texto do botão')
-botao1.setStyleSheet('font-size: 80px;')
+class MyWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-botao2 = QPushButton('Botão 2')
-botao2.setStyleSheet('font-size: 40px;')
+        self.central_widget = QWidget()
 
-botao3 = QPushButton('Botão 3')
-botao3.setStyleSheet('font-size: 40px;')
+        self.setCentralWidget(self.central_widget)
+        self.setWindowTitle('Minha janela bonita')
 
-layout = QGridLayout()
-central_widget.setLayout(layout)
+        # Botão
+        self.botao1 = self.make_button('Texto do botão')
+        self.botao1.clicked.connect(self.segunda_acao_marcada)  # type: ignore
 
-layout.addWidget(botao1, 1, 1, 1, 1)
-layout.addWidget(botao2, 1, 2, 1, 1)
-layout.addWidget(botao3, 3, 1, 1, 2)
+        self.botao2 = self.make_button('Botão 2')
+
+        self.botao3 = self.make_button('Terceiro')
+
+        self.grid_layout = QGridLayout()
+        self.central_widget.setLayout(self.grid_layout)
+
+        self.grid_layout.addWidget(self.botao1, 1, 1, 1, 1)
+        self.grid_layout.addWidget(self.botao2, 1, 2, 1, 1)
+        self.grid_layout.addWidget(self.botao3, 3, 1, 1, 2)
+
+        # statusBar
+        self.status_bar = self.statusBar()
+        self.status_bar.showMessage('Mostrar mensagem na barra')
+
+        # menuBar
+        self.menu = self.menuBar()
+        self.primeiro_menu = self.menu.addMenu('Primeiro menu')
+        self.primeira_acao = self.primeiro_menu.addAction('Primeira ação')
+        self.primeira_acao.triggered.connect(  # type:ignore
+            self.muda_mensagem_da_status_bar)
+
+        self.segunda_action = self.primeiro_menu.addAction('Segunda ação')
+        self.segunda_action.setCheckable(True)
+        self.segunda_action.toggled.connect(  # type:ignore
+            self.segunda_acao_marcada)
+        self.segunda_action.hovered.connect(  # type:ignore
+            self.segunda_acao_marcada)
+
+    @Slot()
+    def muda_mensagem_da_status_bar(self):
+        self.status_bar.showMessage('O meu slot foi executado')
+
+    @Slot()
+    def segunda_acao_marcada(self):
+        print('Está marcado?', self.segunda_action.isChecked())
+
+    def make_button(self, text):
+        btn = QPushButton(text)
+        btn.setStyleSheet('font-size: 80px;')
+        return btn
 
 
-@Slot()
-def slot_example(status_bar):
-    def inner():
-        status_bar.showMessage('O meu slot foi executado')
-    return inner
-
-
-@Slot()
-def outro_slot(checked):
-    print('Está marcado?', checked)
-
-
-@Slot()
-def terceiro_slot(action):
-    def inner():
-        outro_slot(action.isChecked())
-    return inner
-
-
-# statusBar
-status_bar = window.statusBar()
-status_bar.showMessage('Mostrar mensagem na barra')
-
-# menuBar
-menu = window.menuBar()
-primeiro_menu = menu.addMenu('Primeiro menu')
-primeira_acao = primeiro_menu.addAction('Primeira ação')
-primeira_acao.triggered.connect(slot_example(status_bar))  # type:ignore
-
-segunda_action = primeiro_menu.addAction('Segunda ação')
-segunda_action.setCheckable(True)
-segunda_action.toggled.connect(outro_slot)  # type:ignore
-segunda_action.hovered.connect(terceiro_slot(segunda_action))  # type:ignore
-
-botao1.clicked.connect(terceiro_slot(segunda_action))  # type:ignore
-
-window.show()
-app.exec()  # O loop da aplicação
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MyWindow()
+    window.show()
+    app.exec()  # O loop da aplicação
